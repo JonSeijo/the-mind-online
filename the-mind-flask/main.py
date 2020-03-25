76d14676e47f904937d5fa2b312a577a671927f0
jonno <jon.seijo@gmail.com>
2020-03-25 12:43:18 -0300

    Conexion con websockets

--- the-mind-flask/main.py
+++ the-mind-flask/main.py
@@ -1,10 +1,24 @@
+import time
 from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, emit, send
 
 app = Flask("__main__")
+socketio = SocketIO(app, cors_allowed_origins='http://localhost:3000')
+ROOMS = {}
 
 @app.route("/")
 def my_index():
-	return render_template("index.html", flask_token="Hello  world")
+	return "Mejor proba con localhost:3000"
 
-app.run(debug=True)
+@app.route("/time")
+def time_api():
+	return {'time': time.time()}
+
+@socketio.on('connect')
+def on_connect():
+	print("Alguien se conecto")
+	emit('connect_response')
+
+if __name__ == "__main__":
+	socketio.run(app, port=5000, debug=True)
 
--- the-mind-react/src/App.js
+++ the-mind-react/src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
+import io from 'socket.io-client'
 import logo from './logo.svg';
 import './App.css';
 
+const socket = io('http://localhost:5000')
+
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    socket.emit('connect');
+    socket.on('connect_response', () => {
+      setCurrentTime(999);
+    });
+
+    // fetch('/time').then(res => res.json()).then(data => {
+    //   setCurrentTime(data.time);
+    // });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>La hora actual es: {currentTime}</p>
       </header>
     </div>
   );
