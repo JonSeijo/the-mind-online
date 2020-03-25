d27d1cba7cee850bcbaaaf680ac7134915c97de6
jonno <jon.seijo@gmail.com>
2020-03-25 14:59:08 -0300

    Repartir cartas

--- the-mind/model/juego.py
+++ the-mind/model/juego.py
@@ -1,28 +1,46 @@
 # pyre-strict
 
-from typing import List
+import random
+from typing import Dict, List
 
 class Juego(object):
 
-	def __init__(self) -> None:
+	def __init__(self, jugadores: List[str] = []) -> None:
+		self._assert_jugadores_unicos(jugadores)
 		self._nivel: int = 1
-		self._jugadores: List[str] = []
+		self._jugadores: List[str] = jugadores
+		self._repartir_cartas()
 
 	def nivel(self) -> int:
 		return self._nivel
 
 	def subir_nivel(self) -> None:
 		self._nivel += 1
-
-	def agregar_jugador(self, jugador: str) -> None:
-		if jugador in self._jugadores:
-			raise JugadorExistenteException
-
-		self._jugadores.append(jugador)
+		self._repartir_cartas()
 
 	def jugadores(self) -> List[str]:
 		return self._jugadores
 
+	def cartas_por_jugador(self) -> Dict[str, List[int]]:
+		return self._cartas_por_jugador
+
+	def _assert_jugadores_unicos(self, jugadores: List[str]) -> None:
+		if len(jugadores) != len(set(jugadores)):
+			raise JugadorExistenteException
+
+	def _repartir_cartas(self) -> None:
+		mazo = self._mazo_mezclado()
+		self._cartas_por_jugador: Dict[str, List[int]] = {}
+
+		for jug_idx, jug in enumerate(self.jugadores()):
+			inicio = jug_idx * self.nivel()
+			fin = inicio + self.nivel()
+			self._cartas_por_jugador[jug] = mazo[inicio:fin]
+
+	def _mazo_mezclado(self) -> List[int]:
+		mazo = [ i for i in range(1, 101) ]
+		random.shuffle(mazo)
+		return mazo
 
 class JugadorExistenteException(Exception):
 	pass
--- the-mind/tests/test_juego.py
+++ the-mind/tests/test_juego.py
@@ -4,36 +4,67 @@ import unittest
 
 from model.juego import Juego, JugadorExistenteException
 
+def juego_default() -> Juego:
+	return Juego(jugadores=['Articuno', 'Zapdos'])
+
 class JuegoTest(unittest.TestCase):
 
 	def test_juego_empieza_nivel_uno(self) -> None:
-		juego = Juego()
+		juego = juego_default()
 		self.assertEqual(1, juego.nivel())
 
 
 	def test_juego_sube_nivel_correctamente(self) -> None:
-		juego = Juego()
+		juego = juego_default()
 		juego.subir_nivel()
 		juego.subir_nivel()
 		self.assertEqual(3, juego.nivel())
 
 
 	def test_juego_registra_jugadores(self) -> None:
-		juego = Juego()
-		juego.agregar_jugador('Articuno')
-		juego.agregar_jugador('Zapdos')
-
+		juego = Juego(jugadores=['Articuno', 'Zapdos'])
 		jugadores = juego.jugadores()
+
 		self.assertIn('Articuno', jugadores)
 		self.assertIn('Zapdos', jugadores)
 		self.assertEqual(2, len(jugadores))
 
 
-	def test_juego_no_agrega_jugadores_repetidos(self) -> None:
-		juego = Juego()
-		juego.agregar_jugador('Moltres')
-
+	def test_juego_no_registra_jugadores_repetidos(self) -> None:
 		self.assertRaises(
 			JugadorExistenteException,
-			juego.agregar_jugador, 'Moltres'
+			Juego, jugadores=['Moltres', 'Moltres']
 		)
+
+	def test_jugadores_tienen_una_carta_al_inicio(self) -> None:
+		juego = Juego(jugadores=['Articuno', 'Zapdos'])
+		cartas_de = juego.cartas_por_jugador()
+		self.assertEqual(1, len(cartas_de['Articuno']))
+		self.assertEqual(1, len(cartas_de['Zapdos']))
+
+
+	def test_jugadores_tienen_cartas_igual_nivel(self) -> None:
+		juego = Juego(jugadores=['Articuno', 'Zapdos'])
+		juego.subir_nivel()
+		juego.subir_nivel()
+		cartas_de = juego.cartas_por_jugador()
+		self.assertEqual(3, len(cartas_de['Articuno']))
+		self.assertEqual(3, len(cartas_de['Zapdos']))
+
+
+	def test_jugadores_tienen_cartas_distintas(self) -> None:
+		juego = Juego(jugadores=['Articuno', 'Zapdos'])
+		juego.subir_nivel()
+		juego.subir_nivel()
+		cartas_de = juego.cartas_por_jugador()
+
+		cartas_todas = [
+			carta for jug, cartas in cartas_de.items()
+			for carta in cartas
+		]
+
+		self.assertUnique(cartas_todas)
+
+
+	def assertUnique(self, elems) -> None:
+		self.assertEqual(len(elems), len(set(elems)))
