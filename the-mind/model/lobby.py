482e7fc496bf856eb327593e217abf936ed9fcce
jonno <jon.seijo@gmail.com>
2020-03-28 18:29:17 -0300

    Lobby

---
+++ the-mind/model/lobby.py
@@ -0,0 +1,31 @@
+# pyre-strict
+
+from typing import List
+
+
+class Lobby():
+
+	def __init__(self) -> None:
+		self._jugadores : List[str] = []
+
+	def agregar_jugador(self, jugador: str) -> None:
+		if jugador in self._jugadores:
+			raise JugadorExistenteException()
+
+		self._jugadores.append(jugador)
+
+	def remover_jugador(self, jugador: str) -> None:
+		if jugador not in self._jugadores:
+			raise JugadorInexistenteException()
+
+		self._jugadores.remove(jugador)
+
+	def jugadores(self) -> List[str]:
+		return self._jugadores
+
+
+class JugadorExistenteException(Exception):
+	pass
+
+class JugadorInexistenteException(Exception):
+	pass
---
+++ the-mind/tests/test_lobby.py
@@ -0,0 +1,38 @@
+# pyre-strict
+
+import unittest
+
+from model.lobby import (
+	Lobby,
+	JugadorExistenteException,
+	JugadorInexistenteException
+)
+
+class LobbyTest(unittest.TestCase):
+
+	def test_lobby_Agrega_jugadores_correctamente(self) -> None:
+		lobby = Lobby()
+		lobby.agregar_jugador('Articuno')
+		self.assertIn('Articuno', lobby.jugadores())
+
+		lobby.agregar_jugador('Zapdos')
+		self.assertIn('Articuno', lobby.jugadores())
+		self.assertIn('Zapdos', lobby.jugadores())
+
+	def test_lobby_no_agrega_jugador_existente(self) -> None:
+		lobby = Lobby()
+		lobby.agregar_jugador('Articuno')
+		self.assertRaises(JugadorExistenteException,
+			lobby.agregar_jugador, 'Articuno')
+
+	def test_jugador_puede_salir_del_lobby(self) -> None:
+		lobby = Lobby()
+		lobby.agregar_jugador('Articuno')
+		self.assertIn('Articuno', lobby.jugadores())
+		lobby.remover_jugador('Articuno')
+		self.assertNotIn('Articuno', lobby.jugadores())
+
+	def test_jugador_inexistente_no_puede_salir_del_lobby(self) -> None:
+		lobby = Lobby()
+		self.assertRaises(JugadorInexistenteException,
+			lobby.remover_jugador, 'Articuno')
