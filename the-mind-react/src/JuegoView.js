e700da6f9d3f21e484b5688389510d233914722f
jonno <jon.seijo@gmail.com>
2020-04-03 12:33:16 -0300

    Muestro el estado del juego

--- the-mind-react/src/JuegoView.js
+++ the-mind-react/src/JuegoView.js
@@ -4,7 +4,11 @@ class JuegoView extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {'mesa': null}
+    this.state = {
+      'mesa': -1,
+      'cartas': [],
+      'jugadores_cantidades': {},
+    }
   }
 
   componentDidMount() {
@@ -20,16 +24,56 @@ class JuegoView extends React.Component {
 
 
   handleJuegoUpdate(juego_state) {
+    let name = this.props.name
+    let cartas_por_jugador = juego_state.cartas_por_jugador
+    let cartas_mias = cartas_por_jugador[name]
+    let jugadores_cantidades = {}
+
+    console.log("cartas_por_jugador")
+    console.log(cartas_por_jugador)
+
+    Object.entries(cartas_por_jugador).map(
+      ([jug, suscartas]) =>
+        jugadores_cantidades[jug] = suscartas.length
+    )
+
+    console.log("jugadores_cantidades")
+    console.log(jugadores_cantidades)
+
     this.setState({
-      'mesa': juego_state.mesa
+      'mesa': juego_state.mesa,
+      'cartas': cartas_mias,
+      'jugadores_cantidades': jugadores_cantidades
     })
   }
 
   render() {
+
+    let cartasItems = []
+    let cartasRestantes = []
+
+    if (this.state.cartas) {
+      cartasItems = this.state.cartas.map((carta) =>
+        <li key={carta}> {carta} </li>
+      );
+    }
+
+    if (this.state.jugadores_cantidades) {
+      cartasRestantes = Object.entries(this.state.jugadores_cantidades)
+        .map( ([jug, cant]) =>
+          <li key={jug}> {jug + ": " + cant} </li>)
+    }
+
     return (
       <div>
         <div> Estoy en el JUEGO. </div>
-        <div> La mesa es: {this.state.mesa} </div>
+        <div> La carta en la mesa es: {this.state.mesa} </div>
+        <br/>
+        <div> Mis cartas son: </div>
+        <div> <ul>{cartasItems}</ul> </div>
+        <br/>
+        <div> Cant de cartas restantes: </div>
+        <div> <ul>{cartasRestantes}</ul> </div>
       </div>
     )
   }
