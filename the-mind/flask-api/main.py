0d673188ee5152641d64b3fb71e31fa51074c7a1
jonno <jon.seijo@gmail.com>
2020-05-04 22:28:13 -0300

    Utilizo el nuevo MindControl en la API

--- the-mind/flask-api/main.py
+++ the-mind/flask-api/main.py
@@ -6,11 +6,7 @@ from flask_socketio import SocketIO, join_room, emit, send
 
 from model.juego import Juego
 from model.lobby import Lobby
-
-class TheMindApi():
-        def __init__(self) -> None:
-                self.juego = None
-                self.lobby = Lobby()
+from model.mind_control import MindControl
 
 app = Flask("__main__")
 socketio = SocketIO(app,
@@ -23,9 +19,11 @@ socketio = SocketIO(app,
 )
 
 ROOMS = {}
-themind = TheMindApi()
+themind = MindControl()
 
-info_conexiones = {}
+player_name_conexiones = {}
+lobby_name_conexiones = {}
+
 
 @app.route("/")
 def my_index():
@@ -50,20 +48,18 @@ def on_connectar(param):
 
 @socketio.on('connect')
 def on_connect():
-        print("~~~~~~~~~~~~~~~~CONECTE THE MIND")
-        emit('conectar_response', {'status': 'OK'})
+	print("~~~~~~~~~~~~~~~~CONECTE THE MIND")
+	emit('conectar_response', {'status': 'OK'})
 
 
 @socketio.on('disconnect')
 def on_disconnect():
-	name = info_conexiones.pop(request.sid, None)
+	name = player_name_conexiones.pop(request.sid, None)
 	print('--DESCONECTO: ' + str(name))
 
-	themind.juego.terminar()
-	themind.lobby.remover_jugador(name)
-
-	socketio.emit('juego_terminado', themind.lobby.estado())
-	socketio.emit('lobby_update', themind.lobby.estado())
+	lobby_stata = themind.desconectar_jugador(name)
+	socketio.emit('juego_terminado', lobby_stata)
+	socketio.emit('lobby_update', lobby_stata)
 
 
 @socketio.on('lobby_agregar_jugador')
@@ -72,11 +68,14 @@ def on_lobby_agregar_jugador(params):
 		player_name = params['player_name']
 		lobby_name = params['lobby_name']
 
-		themind.lobby.agregar_jugador(player_name)
+		themind.agregar_jugador(player_name, lobby_name)
 
-		info_conexiones[request.sid] = player_name
+		# TODO: Ver que pasa cuando esta conexion ya existe.
+		# Cuando desarrollo local se crean varias?
+		player_name_conexiones[request.sid] = player_name
+		lobby_name_conexiones[request.sid] = lobby_name
 
-		socketio.emit('lobby_update', themind.lobby.estado())
+		socketio.emit('lobby_update', themind.estado_lobby(lobby_name))
 
 	except Exception as ex:
 		emit('lobby_update', {'error': str(ex)})
@@ -84,46 +83,49 @@ def on_lobby_agregar_jugador(params):
 
 @socketio.on('juego_iniciar')
 def on_juego_iniciar():
-	if len(themind.lobby.jugadores()) >= 2:
-		themind.juego = Juego.iniciar(jugadores=themind.lobby.jugadores())
-		socketio.emit('juego_iniciado')
+	lobby_name = lobby_name_conexiones[request.sid]
+	themind.iniciar_juego_en(lobby_name)
+	socketio.emit('juego_iniciado')
 
 
 @socketio.on('poner_carta')
 def on_poner_carta(params):
-	name = info_conexiones.get(request.sid, None)
-	if not name:
+	player_name = player_name_conexiones.get(request.sid, None)
+	lobby_name = lobby_name_conexiones.get(request.sid, None)
+	if not player_name or not lobby_name:
 		return
 
 	carta = int(params['carta'])
-	themind.juego.poner_carta(name, carta)
-	socketio.emit('juego_update', themind.juego.estado())
+
+	themind.colocar_carta(lobby_name, player_name, carta)
+	socketio.emit('juego_update', themind.estado_juego(lobby_name))
 
 
 @socketio.on('subir_nivel')
 def on_subir_nivel():
-	# TODO: try catch aca
-	themind.juego.subir_nivel()
-	socketio.emit('juego_update', themind.juego.estado())
+	lobby_name = lobby_name_conexiones[request.sid]
+	themind.subir_nivel_en(lobby_name)
+	socketio.emit('juego_update', themind.estado_juego(lobby_name))
 
 
 @socketio.on('juego_quiero_terminar')
 def on_juego_quiero_terminar():
-	themind.juego.terminar()
-	socketio.emit('juego_terminado', themind.lobby.estado())
+	lobby_name = lobby_name_conexiones[request.sid]
+	themind.terminar_juego(lobby_name)
+	socketio.emit('juego_terminado', themind.estado_lobby(lobby_name))
 
 
 @socketio.on('lobby_estado')
 def on_lobby_estado():
-	emit('lobby_update', themind.lobby.estado())
+	lobby_name = lobby_name_conexiones[request.sid]
+	emit('lobby_update', themind.estado_lobby(lobby_name))
 
 
 @socketio.on('juego_estado')
 def on_juego_estado():
-	emit('juego_update', themind.juego.estado())
+	lobby_name = lobby_name_conexiones[request.sid]
+	emit('juego_update', themind.estado_juego(lobby_name))
 
 
 if __name__ == "__main__":
-	themind = TheMindApi()
-	print("~~~~~~~~~~~~~~~~EMPEZE THE MIND")
 	socketio.run(app, host='127.0.0.1', port=5000, debug=True)
